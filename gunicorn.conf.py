fda9a06176cece76fe5f3462a4b7edab2375c59e
frizzle0815 <frizzle0815@gmail.com>
2024-12-28 13:13:33 +0100

    Gunicorn update for streaming

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,12 +1,15 @@
 # Gunicorn configuration
+worker_class = 'gevent'
 workers = 4
 bind = "0.0.0.0:8000"
-timeout = 120
 keepalive = 5
-worker_class = "sync"
+
+# Increase timeout for streaming connections
+worker_connections = 1000
+timeout = 300
 
 # Add logging configuration
 loglevel = "info"
 accesslog = "-"  # "-" means stdout
 errorlog = "-"   # "-" means stderr
-access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+access_log_format = '%({x-real-ip}i)s %(l)s %(t)s "%(r)s" %(s)s %(b)s'
